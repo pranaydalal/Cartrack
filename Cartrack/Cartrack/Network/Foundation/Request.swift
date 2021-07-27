1be18a0ec7862fddf123570ed92c30702ad5ac8b
pranaydalal <pranaydalal05@gmail.com>
2021-07-27 23:14:28 +0530

    Created request builder

--- Cartrack/Cartrack/Network/Foundation/Request.swift
+++ Cartrack/Cartrack/Network/Foundation/Request.swift
@@ -10,3 +10,46 @@ import Foundation
 protocol URLRequestConvertible {
     func expressAsURLRequest() -> URLRequest
 }
+
+protocol Request: URLRequestConvertible {
+    var url: URL { get }
+    
+    var endPoint: String { get }
+}
+
+///Just informing that request will be parameterized
+protocol ParameteredRequest: Request {
+    func parameter() -> [String: String]
+    func build(usingBuilder builder: RequestBuilder) throws -> URLRequest
+}
+
+extension ParameteredRequest {
+    func build(usingBuilder builder: RequestBuilder = NetworkRequestBuilder()) throws -> URLRequest {
+        return try builder.buildURLRequest(withURL: self.url, andParameters: self.parameter())
+    }
+    
+    func expressAsURLRequest() throws -> URLRequest {
+        return try self.build()
+    }
+
+    func parameter() -> [String : String] {
+        return [:]
+    }
+    
+}
+
+protocol DataRequest: Request {
+    associatedtype Response: Decodable
+    func execute(onNetwork network: NetworkSession, then completion: @escaping ((Result<Response, Error>) -> Void))
+    
+}
+
+extension DataRequest {
+    func execute(onNetwork network: NetworkSession, then completion: @escaping ((Result<Response, Error>) -> Void)) {
+        
+        network.call(self) { result in
+            
+        }
+    }
+}
+
---
+++ Cartrack/Cartrack/Network/Foundation/RequestBuilder.swift
@@ -0,0 +1,57 @@
+//
+//  RequestBuilder.swift
+//  Cartrack
+//
+//  Created by PRANAY DALAL on 27/07/21.
+//
+
+import Foundation
+
+enum BuilderError: Error {
+    case apiKeyMissing
+    case unableToResolveURL(URL)
+    case unableBuildURL(message: String)
+}
+
+protocol RequestBuilder {
+    
+    func buildURLRequest(with url: URL) -> URLRequest
+    
+    func buildURLRequest(withURL url: URL, andParameters parameters: [String: String]) throws -> URLRequest
+}
+
+struct NetworkRequestBuilder: RequestBuilder {
+    
+    func buildURLRequest(with url: URL) -> URLRequest {
+        return URLRequest(url: url,
+                          cachePolicy: URLRequest.CachePolicy.reloadRevalidatingCacheData,
+                          timeoutInterval: 30)
+    }
+    
+    func buildURLRequest(withURL url: URL, andParameters parameters: [String: String]) throws -> URLRequest {
+        
+        guard var components = URLComponents(url: url,
+                                             resolvingAgainstBaseURL: false) else {
+                                                throw BuilderError.unableToResolveURL(url)
+        }
+        
+        var queryItems = [URLQueryItem]()
+        for key in parameters.keys.sorted() {
+            guard let param = parameters[key] else { continue }
+            queryItems.append(URLQueryItem(name: key, value: param))
+        }
+        
+        components.queryItems = queryItems
+        
+        guard let url = components.url else {
+            let errorMessage = components.queryItems?.map { String(describing: $0) }.joined(separator: ", ") ?? ""
+            
+            throw BuilderError.unableBuildURL(message: "query item \(errorMessage)")
+        }
+        
+        
+        return URLRequest(url: url,
+                          cachePolicy: URLRequest.CachePolicy.reloadRevalidatingCacheData,
+                          timeoutInterval: 30)
+    }
+}
