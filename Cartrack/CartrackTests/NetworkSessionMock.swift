15836363cceeb68bc1d78ca3a121a195a6d60631
pranaydalal <pranaydalal05@gmail.com>
2021-07-28 12:24:27 +0530

    Created Network Session Mock to be used for testcases

---
+++ Cartrack/CartrackTests/NetworkSessionMock.swift
@@ -0,0 +1,43 @@
+//
+//  NetworkSessionMock.swift
+//  CartrackTests
+//
+//  Created by PRANAY DALAL on 28/07/21.
+//
+
+import Foundation
+
+@testable import Cartrack
+
+enum ResponeType {
+    case success(Data)
+    case failure(Error)
+}
+
+class NetworkSessionMock: NetworkSession {
+    
+    enum NetworkSessionMockError: Error {
+        case responseIsNotSet
+    }
+    
+    var request: URLRequestConvertible?
+    var url: URL?
+    var response: ResponeType = .success(Data())
+    
+    
+    func call(_ request: URLRequestConvertible, then completionHandler: @escaping ((Result<Data, Error>) -> Void)) {
+        
+        do {
+            self.request = request
+            self.url = try request.expressAsURLRequest().url
+            switch self.response {
+            case let .success(data):
+                completionHandler(.success(data))
+            case let .failure(error):
+                completionHandler(.failure(error))
+            }
+        } catch {
+            completionHandler(.failure(error))
+        }
+    }
+}
