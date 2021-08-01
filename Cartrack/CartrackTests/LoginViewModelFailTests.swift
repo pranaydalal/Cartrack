69f47355763211c9a0d9313bd1085f1557ac212e
pranaydalal <pranaydalal05@gmail.com>
2021-08-01 23:20:39 +0530

    Implemented TestCases for LoginViewModel

---
+++ Cartrack/CartrackTests/LoginViewModelFailTests.swift
@@ -0,0 +1,61 @@
+//
+//  LoginViewModelFailTests.swift
+//  CartrackTests
+//
+//  Created by PRANAY DALAL on 01/08/21.
+//
+
+import XCTest
+
+@testable import Cartrack
+
+class MockUserDatabaseFailingHandler: UserAccountService {
+    func authenticateUser(withUserName username: String, andPassword password: String) -> Bool {
+        return false
+    }
+    
+    func registerUser(withUserName username: String, andPassword password: String, andCountry country: String) -> Bool {
+        return false
+    }
+}
+
+class LoginViewModelFailTests: XCTestCase {
+    
+    var viewModel : LoginViewModel!
+    var mockService : MockUserDatabaseFailingHandler!
+    
+    override func setUpWithError() throws {
+        self.mockService = MockUserDatabaseFailingHandler()
+        self.viewModel = LoginViewModel(with: mockService)
+    }
+
+    override func tearDownWithError() throws {
+        self.mockService = nil
+        self.viewModel = nil
+    }
+
+    func testLoginFailed() {
+        let failExpectation = expectation(description: "fail")
+        viewModel.didLoginSuccessful = {
+            XCTFail()
+        }
+        viewModel.didLoginFailed = {
+            failExpectation.fulfill()
+        }
+        viewModel.login()
+        waitForExpectations(timeout: 10.0, handler: nil)
+    }
+
+    
+    func testLoginButtonDisabled() {
+        let loginButtonExpectation = self.expectation(description: "update")
+        self.viewModel.country = "India"
+        self.viewModel.userName = "ABC123"
+        self.viewModel.loginButtonEnableStatus = { isEnabled in
+            loginButtonExpectation.fulfill()
+            XCTAssertFalse(isEnabled)
+        }
+        self.viewModel.password = "abc"
+        self.waitForExpectations(timeout: 10.0, handler: nil)
+    }
+}
---
+++ Cartrack/CartrackTests/LoginViewModelTests.swift
@@ -0,0 +1,76 @@
+//
+//  LoginViewModelTests.swift
+//  CartrackTests
+//
+//  Created by PRANAY DALAL on 01/08/21.
+//
+
+import XCTest
+@testable import Cartrack
+
+class MockUserDatabaseHandler: UserAccountService {
+    func authenticateUser(withUserName username: String, andPassword password: String) -> Bool {
+        return true
+    }
+    
+    func registerUser(withUserName username: String, andPassword password: String, andCountry country: String) -> Bool {
+        return true
+    }
+}
+
+class LoginViewModelTests: XCTestCase {
+    
+    var viewModel : LoginViewModel!
+    var mockService : MockUserDatabaseHandler!
+    
+    override func setUpWithError() throws {
+        self.mockService = MockUserDatabaseHandler()
+        self.viewModel = LoginViewModel(with: mockService)
+    }
+
+    override func tearDownWithError() throws {
+        self.mockService = nil
+        self.viewModel = nil
+    }
+
+    func testLoginSuccess() {
+        let successExpectation = expectation(description: "success")
+        self.viewModel.didLoginSuccessful = {
+            successExpectation.fulfill()
+        }
+        self.viewModel.didLoginFailed = {
+            XCTFail()
+        }
+        viewModel.login()
+        self.waitForExpectations(timeout: 10.0, handler: nil)
+    }
+    
+    func testCountry() {
+        let launchExpectation = self.expectation(description: "launch")
+        self.viewModel.country = "Singapore"
+        self.viewModel.didLaunchCountryPicker = { country in
+            XCTAssertEqual(country, "Singapore")
+            launchExpectation.fulfill()
+            self.viewModel.country = "Philippines"
+        }
+        let updateExpectation = self.expectation(description: "update")
+        self.viewModel.didUpdateCountry = { country in
+            XCTAssertEqual(country, "Philippines")
+            updateExpectation.fulfill()
+        }
+        self.viewModel.selectCountry()
+        self.waitForExpectations(timeout: 10.0, handler: nil)
+    }
+    
+    func testLoginButtonEnabled() {
+        let loginButtonExpectation = self.expectation(description: "update")
+        self.viewModel.country = "India"
+        self.viewModel.userName = "ABC123"
+        self.viewModel.loginButtonEnableStatus = { isEnabled in
+            loginButtonExpectation.fulfill()
+            XCTAssertTrue(isEnabled)
+        }
+        self.viewModel.password = "abc@123"
+        self.waitForExpectations(timeout: 10.0, handler: nil)
+    }
+}
