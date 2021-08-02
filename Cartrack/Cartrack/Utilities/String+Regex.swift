2db598acab326cdaf0f45df8cef63ef34b8b80ab
pranaydalal <pranaydalal05@gmail.com>
2021-08-02 12:53:33 +0530

    Created Regex utility with string extension

---
+++ Cartrack/Cartrack/Utilities/String+Regex.swift
@@ -0,0 +1,17 @@
+//
+//  String+Regex.swift
+//  Cartrack
+//
+//  Created by PRANAY DALAL on 02/08/21.
+//
+
+import Foundation
+
+extension String {
+    func doesMatchRegex(_ regexPattern: String) -> Bool {
+        guard let regex = try? NSRegularExpression(pattern: regexPattern, options: .caseInsensitive) else {
+            return false
+        }
+        return regex.matches(in: self, options: [], range: NSMakeRange(0, self.count)).count > 0
+    }
+}
