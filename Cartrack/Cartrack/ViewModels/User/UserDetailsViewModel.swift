18e2156b67b98c37ceb1fbee1249a6e7d0490f3f
pranaydalal <pranaydalal05@gmail.com>
2021-08-02 14:38:47 +0530

    Documentation for UserDetailsViewModel

--- Cartrack/Cartrack/ViewModels/User/UserDetailsViewModel.swift
+++ Cartrack/Cartrack/ViewModels/User/UserDetailsViewModel.swift
@@ -9,12 +9,14 @@ import Foundation
 import CoreLocation
 
 final class UserDetailsViewModel {
-    let user : User
     
-    init(user: User) {
-        self.user = user
-    }
+    //MARK: Private properties
     
+    private let user : User
+    
+    //MARK: Public properties
+    
+    /// Address by concatinating address components with ',\n'
     var address : String {
         let addressContents = [user.address.street ,user.address.suite, user.address.city, user.address.zipcode]
             .compactMap { $0.trimmingCharacters(in: .whitespacesAndNewlines) }
@@ -23,6 +25,7 @@ final class UserDetailsViewModel {
         return addressContents.joined(separator: ",\n")
     }
     
+    /// Location of user
     var location : CLLocation? {
         guard let lat = CLLocationDegrees(user.address.geo.lat),
               let lng = CLLocationDegrees(user.address.geo.lng) else {
@@ -31,23 +34,39 @@ final class UserDetailsViewModel {
         return CLLocation(latitude: lat, longitude: lng)
     }
     
+    /// Complete name of user
     var username: String {
         return user.name
     }
     
+    /// Email address of user
     var email: String {
         return user.email
     }
     
+    /// Phone number of user
     var phone: String {
         return user.phone
     }
     
+    /// Website of user
     var website: String {
         return user.website
     }
     
+    /// Organisation of user
     var organisation: String {
         return user.company.name
+    }
+    
+    //MARK: Initializers
+    
+    /**
+     To initialize with user
+     
+     - parameter user: user to be used for getting user details
+     */
+    init(user: User) {
+        self.user = user
     }
 }
