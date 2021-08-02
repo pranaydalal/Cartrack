81b1f0c891e377e96af6427288a53d589ef48697
pranaydalal <pranaydalal05@gmail.com>
2021-08-02 15:12:46 +0530

    Documentation for CountryPickerViewModel

--- Cartrack/Cartrack/ViewModels/Login/CountryPickerViewModel.swift
+++ Cartrack/Cartrack/ViewModels/Login/CountryPickerViewModel.swift
@@ -9,37 +9,51 @@ import Foundation
 
 final class CountryPickerViewModel {
     
+    // MARK: Callbacks or observers
+    
     var didUpdateCountry:((String)->())?
     var didDismiss:(()->())?
     
+    //MARK: Private properties
+    
+    private var countrySelection : [String] {
+        return Locale.isoRegionCodes.compactMap { Locale.current.localizedString(forRegionCode: $0) }
+    }
+    
+    //MARK: Public properties
+    
+    /// Holds the current selected country
     var country : String = "" {
         didSet {
             self.didUpdateCountry?(country)
         }
     }
     
-    private var countrySelection : [String] {
-        return Locale.isoRegionCodes.compactMap { Locale.current.localizedString(forRegionCode: $0) }
-    }
+    //MARK: Public methods
     
+    /// To get the Index of current selected country
     func selectedRow() -> Int? {
         return self.countrySelection.firstIndex(of: country)
     }
     
+    /// To get the number of countries
     func numberOfRows() -> Int {
         return self.countrySelection.count
     }
     
+    /// To get the country at index
     func titleForRow(row: Int) -> String? {
         guard row >= 0 && row < self.countrySelection.count else { return nil }
         return self.countrySelection[row]
     }
     
+    /// Select the country at index
     func select(row: Int) {
         guard let title = self.titleForRow(row: row) else { return }
         self.country = title
     }
     
+    /// Dismiss the picker
     func dismiss() {
         self.didDismiss?()
     }
